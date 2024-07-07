b8426503ddcc35ab8656b16aec82a6be993a20c3
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-07-07 19:40:22 +0200

    feat: home-page (#5)

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -5,31 +5,36 @@ import {
   TouchableOpacity,
   ImageSourcePropType,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 interface CategoryCardProps {
   title: string;
   icon: ImageSourcePropType;
   onSelect: () => void;
-  selected: boolean;
 }
 
 const CategoryCard: React.FC<CategoryCardProps> = ({
   title,
   icon,
   onSelect,
-  selected,
 }) => {
+  const [selected, setSelected] = useState<boolean>(false);
+
+  const handlePress = () => {
+    setSelected(!selected);
+    onSelect();
+  };
+
   return (
-    <TouchableOpacity className="items-center mx-2" onPress={onSelect}>
+    <TouchableOpacity className="items-center mx-2" onPress={handlePress}>
       <View
-        className={`border rounded-full p-3 items-center justify-center w-16 h-16 ${
+        className={`border rounded-full p-3 items-center justify-center w-14 h-14 ${
           selected ? "bg-secondary border-white border-2 shadow-md" : ""
         }`}
       >
-        <Image source={icon} className="w-8 h-8 tint-black" />
+        <Image source={icon} className="w-7 h-7 tint-black" />
       </View>
-      <Text className="mt-2 text-sm text-black">{title}</Text>
+      <Text className="mt-2 text-xs text-black">{title}</Text>
     </TouchableOpacity>
   );
 };
