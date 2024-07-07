e30213d22f37d7e6661a3fe11d8b65744df9e86b
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-07-07 15:33:49 +0200

    feat: file-structure

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SavedScreen = () => {
+  return (
+    <View>
+      <Text>SavedScreen</Text>
+    </View>
+  );
+};
+
+export default SavedScreen;
