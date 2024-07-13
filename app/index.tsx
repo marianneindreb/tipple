ba7d7c49b3a2b054993e199a8a471378929bfcf7
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-07-13 23:45:43 +0200

    feat: welcome-page

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,36 @@
 import { Link } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants/images";
+import { View, ImageBackground, Text, TouchableOpacity } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <Link href="/home">Go to Home</Link>
-    </SafeAreaView>
+    <View className="flex-1 ">
+      <ImageBackground
+        source={images.welcomeBackground}
+        resizeMode="cover"
+        className="flex-1 justify-center"
+      >
+        <View className="p-8 relative z-10 space-y-4">
+          <Text className="text-4xl font-bold text-white">tipple.</Text>
+          <Text className="text-2xl text-white">
+            Discover, create, and savor the perfect drink with tipple.
+          </Text>
+          <TouchableOpacity className="align-middle bg-white p-2 rounded-md mt-4">
+            <Text className="text-center font-bold text-base">SIGN UP</Text>
+          </TouchableOpacity>
+          <Text className="text-white text-center mt-4">
+            Already have an account?
+          </Text>
+          <Link
+            className={" text-white font-bold text-center text-base"}
+            href={"./home"}
+          >
+            LOG IN
+          </Link>
+        </View>
+      </ImageBackground>
+      <StatusBar style="light" />
+    </View>
   );
 }
